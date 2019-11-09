b9f388e7352043acb394684aeab541fbc40300fb
Maxim Maslin <zapzarap@yandex.ru>
2019-11-09 11:44:14 +0300

    add chapter 5 solution last exercise

---
+++ Eloquent JavaScript/Chapter 5/Script Direction/script_direction.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+ 
+var SCRIPTS = require('./scripts.js');
+ 
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+function countBy(items, groupName){
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++; }
+        }
+    return counts;
+}
+
+function countDirection(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : "none"; }).filter(({name}) => name != "none");
+    
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+    
+    return scripts.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+}
+
+console.log(countDirection('"woof", "тяв"'))
+// → 61% Han, 22% Latin, 17% Cyrillic
